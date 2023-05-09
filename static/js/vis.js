081ef555f90054b48ffd2068a64bf37dc02ed7e4
Nicholas Pfaff <contact@nicholaspfaff.ch>
2023-05-09 18:21:36 -0400

    Add mustard bottle collision geometries

--- static/js/vis.js
+++ static/js/vis.js
@@ -3,6 +3,10 @@ $(function () {
         $('.visual').hide();
         $('.visual.' + $(this).val()).show();
     });
+    $('#mesh-selector').change(function () {
+        $('.mesh').hide();
+        $('.mesh.' + $(this).val()).show();
+    });
     $('#collision-selector').change(function () {
         $('.collision').hide();
         $('.collision.' + $(this).val()).show();
