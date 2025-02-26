83e9a2d38ba2c986cc2de014314f4a39fbeb593b
Evelyn Fu <evelynfu@mit.edu>
2025-02-25 22:24:45 -0500

    fix default model-viewer

--- static/js/vis.js
+++ static/js/vis.js
@@ -3,6 +3,34 @@ $(function () {
         $('.visual').hide();
         $('.visual.' + $(this).val()).show();
     });
+    let defaultModel = "large_mustard"; // Set default model
+    let defaultMesh = $('.mesh.' + defaultModel);
+
+    // Load the default model into the div
+    defaultMesh.html(`
+        <div class="columns is-centered">
+            <div class="column">
+                <div class="content is-centered">
+                    <model-viewer class="is-centered" style="height: 480px; width: 480px;" 
+                        alt="Raw Mesh" field-of-view="45"
+                        src="./static/models/${defaultModel}.glb" ar camera-controls 
+                        auto-rotate rotation-per-second="30deg" camera-orbit="auto auto 5%" 
+                        ar-status="not-presenting">
+                    </model-viewer>
+                </div>
+            </div>
+            <div class="column">
+                <div class="content is-centered">
+                    <model-viewer class="is-centered" style="height: 480px; width: 480px;" 
+                        alt="Collision Geometry" field-of-view="45"
+                        src="./static/models/${defaultModel}_convex.glb" ar camera-controls 
+                        auto-rotate rotation-per-second="30deg" camera-orbit="auto auto 5%" 
+                        ar-status="not-presenting">
+                    </model-viewer>
+                </div>
+            </div>
+        </div>
+    `);
     $('#mesh-selector').change(function () {
         let selectedValue = $(this).val(); // Get the selected model
         let selectedMesh = $('.mesh.' + selectedValue);
